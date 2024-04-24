8279ae089f60ed339ac278fa6c48811830cf6c8a
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-24 23:14:32 +0700

    Introduction to Python for loop statement with the range() function

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Python for Loop with Range
 
 - Reference: https://www.pythontutorial.net/python-basics/python-for-range
+
+- Run script
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+  # Introduction to Python for loop statement with the range() function
+
+  # for index in range(n):
+  #   statement
+
+  for index in range(5):
+    print(index)
+
+  for index in range(5):
+    print(index + 1)
