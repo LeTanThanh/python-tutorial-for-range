2e5c686af35324c81aa86060f21f3e363f0d5be2
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-24 23:14:32 +0700

    Introduction to Python for loop statement with the range() function

---
+++ main.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+  # Introduction to Python for loop statement with the range() function
+
+  # for index in range(n):
+  #   statement
+
+  for index in range(5):
+    print(index)
