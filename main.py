5532b0269a3ef7e2cba95344572dd88c52b88aba
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-24 23:23:10 +0700

    Specifying the starting value for the sequence

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 if __name__ == "__main__":
   # Introduction to Python for loop statement with the range() function
 
-  # for index in range(n):
-  #   statement
+  """
+  for index in range(n):
+    statement
+  """
 
   for index in range(5):
     print(index)
 
   for index in range(5):
     print(index + 1)
+
+  # Specifying the starting value for the sequence
+
+  """
+  range(start, stop)
+  """
+
+  for index in range(1, 6):
+    print(index)
